a3001c0bbfb297ad055719e34958d67a675b434c
陈 锌 <1436667636@qq.com>
2019-06-06 23:55:21 +0800

    测试

--- application/api/model/UserInvoice.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace app\api\model;
-
-class UserInvoice extends \think\Model
-{
-    // 设置当前模型对应的完整数据表名称
-    protected $table = 'oa_user_invoice';
-}
--- public/admin-vue/config/prod.env.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-module.exports = {
-  NODE_ENV: '"production"',
-  IMAGE_PREFIX: '"https://cdn3.ibingniao.com/"'
-}
